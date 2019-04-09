88bbbe08c8ed06f7c62e060afbb12ad4f7d96596
justinhopper <justin.hopper12@gmail.com>
2019-04-09 11:54:18 -0700

    Initial commit, integration with CircleCI with Packer

---
+++ scripts/basic_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "--- Initial package update"
+sudo apt-get update
+
+echo "--- Install basic system tool packages"
+sudo apt-get install -y apt-transport-https curl gnupg2 software-properties-common
+
+echo "--- Add kubernetes repo"
+sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+echo "--- Import docker key"
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+echo "--- Add repo for docker"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+echo "--- Update package information"
+sudo apt update
+
+echo "--- Install docker"
+sudo apt install -y docker-ce

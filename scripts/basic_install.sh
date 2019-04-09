e50add91d3ced9b54fe1dd007ab34096ee23cf2b
justinhopper <justin.hopper12@gmail.com>
2019-04-09 16:04:58 -0700

    Fixed kubernetes key import

--- scripts/basic_install.sh
+++ scripts/basic_install.sh
@@ -10,11 +10,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl 
 echo "--- Add kubernetes repo"
 sudo /bin/bash -c 'echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'; echo "OK"
 
-echo "--- Import docker key"
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "--- Import kubernetes key"
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 echo "--- Add repo for docker"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"; echo "OK"
+
+echo "--- Import docker key"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo "--- Update package information"
 sudo DEBIAN_FRONTEND=noninteractive apt update

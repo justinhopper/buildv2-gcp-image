99c999a54a209658a11afaf3c8ca908d845f20ba
justinhopper <justin.hopper12@gmail.com>
2019-04-10 11:49:26 -0700

    Fix apt with more setup files

--- scripts/basic_install.sh
+++ scripts/basic_install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+echo "---- set locale"
+sudo locale-gen C.UTF-8 || true
+sudo update-locale LANG=en_US.UTF-8
+sudo /bin/bash -c 'echo "export LANG=C.UTF-8" >> /etc/skel/.bashrc'
+
+echo "---- make Apt non interactive"
+sudo /bin/bash -c 'echo "force-confnew" >> /etc/dpkg/dpkg.cfg'
+sudo /bin/bash -c 'cat /tmp/dpkg.cfg.update >> /etc/sudoers.d/env_keep'
+sudo cp /tmp/apt.conf.update /etc/apt/apt.conf
+
 echo "--- Initial package update"
 sudo DEBIAN_FRONTEND=noninteractive apt update
 
